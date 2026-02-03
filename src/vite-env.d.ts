30d7ef362c849313e693ab023005697e077b71fa
Suchetamon27 <suchetamonbol@gmail.com>
2026-02-03 18:12:12 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+  readonly VITE_APP_ID: string
+  readonly VITE_API_ENV: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
